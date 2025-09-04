188be40824d8b22329afc0e8fe3becca22a09a4f
Thanisorn <thanisorn.sir@gmail.com>
2025-09-04 12:15:11 +0700

    feat: add question creation API with validation middleware

--- app.mjs
+++ app.mjs
@@ -1,5 +1,6 @@
 import express from "express";
 import connectionPool from "./utils/db.mjs";
+import ValidationQuestionData from "./middleware/questionValidation.mjs";
 
 const app = express();
 const port = 4000;
@@ -8,6 +9,35 @@ app.use(express.json());
 
 app.get("/test", (req, res) => {
   return res.json("Server API is working 🚀");
+});
+
+/** CREATE QUESTION */
+app.post("/questions",  ValidationQuestionData, async (req, res) => {
+  const newQuestion = {
+    title: req.body.title,
+    description: req.body.description,
+    category: req.body.category
+  };
+
+  try {
+    const result = await connectionPool.query(
+      `INSERT INTO questions (title, description, category)
+       VALUES ($1, $2, $3)
+       RETURNING id`,
+      [newQuestion.title, newQuestion.description, newQuestion.category]
+    );
+
+    return res.status(201).json({
+      message: "Question created successfully.",
+      id: result.rows[0].id, // DB สร้างให้เอง
+    });
+  } catch (error) {
+    console.error("Error in POST /questions:", error.message);
+    return res.status(500).json({
+      message: "Unable to create question.",
+      error: error.message,
+    });
+  }
 });
 
 app.listen(port, () => {
